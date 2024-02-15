aed9f5b02ad64fe634de5781412a09e861324209
NIGHT666B <mexrami824@gmail.com>
2024-02-15 07:46:09 -0500

    Initial commit

---
+++ app/src/main/java/com/brayan/jose/rodriguez/landazuri/calcularnotas/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.brayan.jose.rodriguez.landazuri.calcularnotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        class estudiantes(){
+        val nombre  : String,
+        val nota : Array<Double>,
+        val porcentaje : Array<Int>
+
+        fun cNotas(): Double {
+            val mbappe = 0
+            val indice = 0
+
+            for (n in nota) {
+                +=
+            }
+
+        } }}
+
+
+
+
+
+
+
+
+    }
+}
